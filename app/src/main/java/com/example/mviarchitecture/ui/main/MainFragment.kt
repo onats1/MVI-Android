ae3d694ef38ef145a5ea3dc8767a56cfa3930e8b
onats1 <donatemowo@yahoo.com>
2019-09-27 15:35:33 +0100

    Created view states and view model structure.

--- app/src/main/java/com/example/mviarchitecture/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mviarchitecture/ui/main/MainFragment.kt
@@ -3,9 +3,15 @@ package com.example.mviarchitecture.ui.main
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.mviarchitecture.R
+import java.lang.Exception
+
 
 class MainFragment : Fragment(){
+
+    lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +27,43 @@ class MainFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = activity?.run {
+            ViewModelProvider(this).get(MainViewModel::class.java)
+        }?: throw Exception("Invalid Activity.")
+
+        subscribeObservers()
     }
+
+    private fun subscribeObservers(){
+
+        viewModel.dataState.observe(viewLifecycleOwner, Observer {dataState ->
+
+            println("DEBUG: Data state ${dataState}")
+            dataState.blogPosts?.let {
+
+            }
+
+            dataState.user?.let {
+
+            }
+
+        })
+
+        viewModel.viewState.observe(viewLifecycleOwner, Observer { dataState ->
+
+            dataState.blogPosts?.let {
+
+            }
+
+            dataState.user?.let {
+
+            }
+        })
+    }
+
+
+
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
--- app/src/main/java/com/example/mviarchitecture/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/mviarchitecture/ui/main/MainViewModel.kt
@@ -2,11 +2,64 @@ package com.example.mviarchitecture.ui.main
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.example.mviarchitecture.models.BlogPost
+import com.example.mviarchitecture.models.User
+import com.example.mviarchitecture.ui.main.state.MainStateEvent
 import com.example.mviarchitecture.ui.main.state.MainViewState
+import com.example.mviarchitecture.utils.AbsentLiveData
 
 class MainViewModel : ViewModel(){
 
+    private val _stateEvent: MutableLiveData<MainStateEvent> = MutableLiveData()
     private val _viewState:  MutableLiveData<MainViewState> = MutableLiveData()
+
     val viewState: LiveData<MainViewState>  get() = _viewState
+    val dataState: LiveData<MainViewState> = Transformations
+        .switchMap(_stateEvent){
+            handleStateEvent(it)
+        }
+
+    private fun handleStateEvent(stateEvent: MainStateEvent): LiveData<MainViewState>{
+
+        return when(stateEvent){
+            is MainStateEvent.GetUserEvent -> {
+                AbsentLiveData.create()
+            }
+
+            is MainStateEvent.GetBlogPostEvent -> {
+                AbsentLiveData.create()
+            }
+
+            is MainStateEvent.None -> {
+                AbsentLiveData.create()
+            }
+        }
+
+
+    }
+
+    fun setBlogListData(blogPost: List<BlogPost>){
+        val update = getCurrentViewStateOrNew()
+        update.blogPosts = blogPost
+        _viewState.value = update
+    }
+
+    fun setBlogUser(blogUser: User){
+        val update = getCurrentViewStateOrNew()
+        update.user = blogUser
+        _viewState.value = update
+    }
+
+
+    fun getCurrentViewStateOrNew(): MainViewState{
+        return viewState.value?.let {
+            it
+        }?: MainViewState()
+    }
+
+    fun setStateEvent(event: MainStateEvent){
+        _stateEvent.value = event
+    }
 }
